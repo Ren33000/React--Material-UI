7cd8728cd9e87ff9b56c8726e19ef4bb901aeb64
Mylene <pont.mylene@gmail.com>
2022-01-15 13:55:31 +0900

    practice palette, customized styling with button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Button } from "@material-ui/core";
+import { Person } from "@material-ui/icons"; 
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
 const App = () => {
-  return <div>Hello world!</div>;
+  const classes = useStyles()
+  return <div>
+    
+  </div>;
 };
 
 export default App;
