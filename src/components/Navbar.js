08d94d445d6eaa89d88e0f4a30d5910228a38bc3
Mylene <pont.mylene@gmail.com>
2022-01-18 17:48:19 +0900

    badges, avatar, responsive search bar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, makeStyles, InputBase, alpha, Badge, Avatar } from "@material-ui/core";
+import { Mail, Search, Notifications } from "@material-ui/icons";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
+    toolbar: {
+        display: "flex",
+        justifyContent: "space-between"
+    },
     logoLg: {
         display: "none",
         [theme.breakpoints.up("sm")]: {
@@ -12,19 +18,69 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.up("sm")]: {
             display: "none",
         },
-    }
+    },
+    search: {
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        borderRadius: theme.shape.borderRadius,
+        width: "50%",
+        [theme.breakpoints.down("sm")]: {
+            display: (props) => (props.open ? "flex" : "none"),
+        },
+    },
+    input: {
+        color: "white",
+        marginLeft: theme.spacing(1),
+    },
+    searchButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up("sm")]: {
+            display: "none",
+        },
+        
+    },
+    icons: {
+        alignItems: "center",
+        display: (props) => (props.open ? "none" : "flex")
+    },
+    badge: {
+        marginRight: theme.spacing(2)
+    },
+    
 }));
 
 const Navbar = () => {
-  const classes = useStyles()
-  return (
-      <AppBar>  
-          <Toolbar> 
-            <Typography variant="h6" className={classes.logoLg}>Lama Dev</Typography>
-            <Typography variant="h6" className={classes.logoSm}>LAMA</Typography>
-          </Toolbar>
-      </AppBar>
-  )
+    const [open, setOpen] = useState(false);
+    const classes = useStyles({ open })
+    return (
+        <AppBar>  
+            <Toolbar className={classes.toolbar}> 
+                <Typography variant="h6" className={classes.logoLg}>Lama Dev</Typography>
+                <Typography variant="h6" className={classes.logoSm}>LAMA</Typography>
+                <div className={classes.search}>
+                    <Search/>
+                    <InputBase placeholder="Search..." className={classes.input}/>
+                </div>
+                <div className={classes.icons}>
+                    <Search 
+                        className={classes.searchButton}
+                        onClick={() => setOpen(true)}
+                    />
+                    <Badge badgeContent={4} color="secondary" className={classes.badge}>
+                        <Mail/>
+                    </Badge>
+                    <Badge badgeContent={2} color="secondary" className={classes.badge}>
+                        <Notifications/>
+                    </Badge>
+                    <Avatar alt="Ren33000" src="avatar.png"/>
+                </div>
+            </Toolbar>
+        </AppBar>
+    )
 };
 
 export default Navbar;
