d9fc0528c8fa995e81bd8c865928235d36df4a81
Mylene <pont.mylene@gmail.com>
2022-01-24 13:15:20 +0900

    added modal

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,22 +1,42 @@
-import { Fab, makeStyles, Tooltip } from "@material-ui/core";
+import { Container, Fab, makeStyles, Modal, Tooltip } from "@material-ui/core";
 import { AddCircleOutline } from "@material-ui/icons";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
     fab: {
         position: "fixed",
         bottom: 10,
         right: 20,
+    },
+    container: {
+        width: 500,
+        height: 550,
+        backgroundColor: "white",
+        position: "absolute",
+        top: 0, 
+        bottom: 0,
+        left: 0,
+        right: 0,
+        margin: "auto",
+
     }
 }));
 
 const Add = () => {
-    const classes = useStyles()
+    const classes = useStyles();
+    const [open, setOpen] = useState(false)
+
     return (
-        <Tooltip title="Add" arial-label="add">
-            <Fab color="primary" className={classes.fab}>
-            <AddCircleOutline />
-            </Fab>
-        </Tooltip>
+        <>
+            <Tooltip title="Add" arial-label="add" onClick={() => setOpen(true)}>
+                <Fab color="primary" className={classes.fab}>
+                <AddCircleOutline />
+                </Fab>
+            </Tooltip>
+            <Modal open={open}>
+                <Container className={classes.container}>My Modal</Container>
+            </Modal>
+        </>
     )
 };
 
