ed8d4c39b639c96292edae322d8ba47ed89cf11f
Mylene <pont.mylene@gmail.com>
2022-01-26 16:40:44 +0900

    alert in modal created

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,6 +1,8 @@
-import { Button, Container, Fab, FormControlLabel, FormLabel, makeStyles, MenuItem, Modal, Radio, RadioGroup, TextField, Tooltip } from "@material-ui/core";
+import { Button, Container, Fab, FormControlLabel, FormLabel, makeStyles, MenuItem, Modal, Radio, RadioGroup, Snackbar, TextField, Tooltip } from "@material-ui/core";
 import { AddCircleOutline } from "@material-ui/icons";
 import { useState } from "react";
+import MuiAlert from '@material-ui/lab/Alert';
+
 
 const useStyles = makeStyles((theme) => ({
     fab: {
@@ -31,9 +33,22 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const Add = () => {
     const classes = useStyles();
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [openAlert, setOpenAlert] = useState(false)
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+    }
+
+    setOpenAlert(false);
+    };
 
     return (
         <>
@@ -76,14 +91,20 @@ const Add = () => {
                             </RadioGroup>
                         </div>
                         <div className={classes.item}>
-                            <Button variant="outlined" color="primary" style={{ marginRight: 20}}>Create</Button>
+                            <Button variant="outlined" color="primary" style={{ marginRight: 20}} onClick={() => setOpenAlert(true)}>Create</Button>
                             <Button variant="outlined" color="secondary" onClick={() => setOpen(false)}>Cancel</Button>
                         </div>
                     </form>
                 </Container>
             </Modal>
+            <Snackbar open={openAlert} autoHideDuration={4000} onClose={handleClose} anchorOrigin={{ vertical: "bottom", horizontal: "left"}}>
+                <Alert onClose={handleClose} severity="success">
+                    Message created!
+                </Alert>
+            </Snackbar>
         </>
     )
 };
 
 export default Add;
+
